239c96a09b72c96d3ccd7499b2bc22e4393ccbaf
BMustafa97 <bilalmustafa1797@gmail.com>
2025-11-14 14:41:39 +0000

    added in external links

--- iOS/CentralMosqueRochdale/Views/ContentView.swift
+++ iOS/CentralMosqueRochdale/Views/ContentView.swift
@@ -54,6 +54,12 @@ struct ContentView: View {
                                 .environmentObject(themeManager)
                         }
                         .buttonStyle(PlainButtonStyle())
+
+                        NavigationLink(destination: ExternalLinksView().environmentObject(themeManager)) {
+                            FeatureRow(icon: "link.circle", title: "Connect", description: "YouTube, WhatsApp & Live Stream")
+                                .environmentObject(themeManager)
+                        }
+                        .buttonStyle(PlainButtonStyle())
                         
                         
                         NavigationLink(destination: NotificationSettingsView().environmentObject(themeManager)) {
